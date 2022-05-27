cf75c824c7cb3bf6ab721647158a66a9b0b071ac
eddahbihossine <eddahbi.hossine.solicode@gmail.com>
2022-05-27 06:42:25 +0000

    jasmine testing guide

---
+++ index.js
@@ -0,0 +1,31 @@
+// // // this repo is for the jasmine presentation created by hossine eddahbi
+// // -----------------------------------------    
+// //1-------------- Jasmine is the most popular JS library for unit testing web apps. In this tutorial, designed for beginners, we’ll present you  with a quick and complete guide to testing with Jasmine.
+
+("jasmine use")
+
+// - jasmine can be used on Node.js as a Cli tool 
+// -jasmine can be used as a library on node.js
+// - jasmine can be used to test a python code as a CLI script (command line interface)
+// jasmine can be used also on ruby  by jamsine .gem
+("jasmine installation tools")
+"there are many  ways to install jasmine we can list :"
+// ("installing jasmine by downloading it from the release page" ) 
+        https://github.com/jasmine/jasmine/releases
+
+
+// installing jasmine by CLI METHOD ON Node.js
+"npm install -g jasmine"
+"$ mkdir jasmine-project $ cd jasmine-project"
+"npm install --save-dev jasmine"
+"npx jasmine init"
+
+// 
+
+
+
+
+ ("here's a quick demonstration");
+
+
+
